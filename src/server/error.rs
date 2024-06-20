16d81814fe85f556c8cd8bd3ed43e6192887035f
wangfiox <wangfiox@gmail.com>
2024-06-21 02:42:52 +0800

    modified:   src/server/error.rs 	modified:   src/server/session.rs

--- src/server/error.rs
+++ src/server/error.rs
@@ -3,15 +3,16 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum ServerError {
     #[error("Failed to bind the address: {0}")] IOError(#[from] std::io::Error), // IO 错误，原因：绑定地址失败
-    #[error("session: {0}")] SessionError(SessionErr), // 会话错误
     #[error("send error: {0}")] SendError(#[from] tokio_tungstenite::tungstenite::Error),
-    #[error("send error: {0}")] ClientErr(ClientErr),
+    #[error("session: {0}")] SessionError(SessionErr), // 会话错误
+    #[error("client error: {0}")] ClientErr(ClientErr),
 }
 
 #[derive(Error, Debug)]
 pub enum SessionErr {
-    #[error("too many clients in a session, expect 2")] TooManyClients,
-    #[error("less than 2 clients in a session, expect 2")] LessThanTwoClients,
+    #[error("send error: {0}")] SendError(#[from] tokio_tungstenite::tungstenite::Error),
+    #[error("serde error: {0}")] SerdeError(#[from] serde_json::Error),
+    #[error("channel error: {0}")] ChannelError(#[from] tokio::sync::mpsc::error::TryRecvError),
 }
 
 #[derive(Error, Debug)]
@@ -22,4 +23,5 @@ pub enum ClientErr {
     #[error("channel send error: {0}")] ChannelSendNullError(
         #[from] tokio::sync::mpsc::error::SendError<()>,
     ),
+    #[error("serde error: {0}")] SerdeError(#[from] serde_json::Error),
 }
--- src/server/session.rs
+++ src/server/session.rs
@@ -1,18 +1,19 @@
 use futures_util::{ SinkExt, StreamExt };
+use serde_json::Value;
 use tokio::{ net::{ TcpStream }, sync::mpsc::{ self, UnboundedSender }, task::JoinHandle };
 use tokio_tungstenite::{ accept_async, tungstenite::WebSocket, WebSocketStream };
 use tokio_tungstenite::tungstenite::protocol::Message;
 use tokio::sync::{ Mutex };
-use std::{ sync::Arc };
+use std::{ collections::HashMap, sync::Arc };
 use serde::{ Serialize, Deserialize };
 use uuid::Uuid;
 
-use super::{ ClientErr, ServerError };
+use super::{ ClientErr, ServerError, SessionErr };
 
-type Conn = Arc<Mutex<WebSocketStream<TcpStream>>>;
+type Socket = Arc<Mutex<WebSocketStream<TcpStream>>>;
 
 struct Client {
-    conn: Conn,
+    conn: Socket,
     score: i32, // java 只有 有符号整数，因此我们只用有符号整数就行了
     thread: JoinHandle<Result<(), ClientErr>>, // 线程句柄，返回 ()
 }
@@ -21,82 +22,56 @@ struct Client {
 struct Session {
     ssid: Uuid,
     clients: Arc<Mutex<(Client, Client)>>,
-    thread: JoinHandle<()>,
+    thread: JoinHandle<Result<(), SessionErr>>,
 }
 
-// /// 这个应该需要一个 Arc Mutex
-// type Sessions = Arc<Mutex<HashMap<Uuid, Session>>>;
-
-// impl Session {
-//     /// 获取到了 session 一个 client 的分数
-//     async fn collect_scores(&self) {
-//         unimplemented!()
-//     }
-
-//     /// 给 client 看对手的分数
-//     async fn refresh_scores(&self) {
-//         // let mut clients = self.clients.lock().await;
-//         // let scores = self.scores.lock().await;
-//         // if let (Some(ref mut client1), Some(ref mut client2)) = *clients {
-//         //     if let (Some(score1), Some(score2)) = *scores {
-//         //         let msg1 = serde_json::to_string(&score2).unwrap();
-//         //         let msg2 = serde_json::to_string(&score1).unwrap();
-//         //         client1.send(Message::Text(msg1)).await.unwrap();
-//         //         client2.send(Message::Text(msg2)).await.unwrap();
-//         //     }
-//         // }
-//         unimplemented!()
-//     }
-
-//     /// 跑 session
-//     async fn run(&self) {
-//         // 宣布开始游戏
-
-//         // 收集分数
-
-//         // 刷新分数
-
-//         // 结束游戏
-//         unimplemented!()
-//     }
-// }
-
-#[derive(Serialize, Deserialize)]
+/// 从 Server 发出去是 "begin"
+/// Client 在没有收到 "begin"
+#[derive(Serialize, Deserialize, Debug)]
 struct GameBegin {
-    /// 从 Server 发出去是 "begin"
-    /// Client 在没有收到 "begin"
-    message: String,
+    begin: String,
 }
 
-#[derive(Serialize, Deserialize)]
+/// 来自于 Client 只能是 "end"
+/// 从 Server 发出去是 "wait" 或者是 "end"
+#[derive(Serialize, Deserialize, Debug)]
 struct GameEnd {
-    /// 来自于 Client 只能是 "end"
-    /// 从 Server 发出去是 "wait" 或者是 "end"
-    message: String,
+    end: String,
+    score: i32,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Score {
-    /// 来自于 Client
-    message: i32,
+    score: i32,
 }
 
 impl Client {
+    /// 主要是管道，专门用来接收的
     async fn handle_client(
-        conn: Conn,
+        conn: Socket,
         score_tx: UnboundedSender<i32>,
         end_tx: UnboundedSender<()>
     ) -> Result<(), ClientErr> {
         while let Some(msg) = conn.lock().await.next().await {
             match msg {
                 Ok(Message::Text(text)) => {
-                    let value: serde_json::Value = serde_json::from_str(&text).unwrap();
-                    if value["message_type"] == "score" {
-                        let score: i32 = value["message"].as_i64().unwrap() as i32;
-                        score_tx.send(score)?;
-                    } else if value["message_type"] == "end" {
+                    let value: Value = serde_json::from_str(&text)?;
+
+                    if value.get("begin").is_some() {
+                        let game_begin: GameBegin = serde_json::from_value(value)?;
+                        dbg!(&game_begin);
+                        // 处理 game_begin 消息
+                    } else if value.get("end").is_some() {
+                        let game_end: GameEnd = serde_json::from_value(value)?;
                         end_tx.send(())?;
+                        dbg!(&game_end);
                         break;
+                    } else if value.get("score").is_some() {
+                        let score: Score = serde_json::from_value(value)?;
+                        score_tx.send(score.score)?;
+                        dbg!(&score);
+                    } else {
+                        dbg!("Unknown message type");
                     }
                 }
                 _ => {
@@ -145,61 +120,7 @@ impl Session {
         let session_thread = {
             let clients = clients.clone();
             tokio::spawn(async move {
-                loop {
-                    tokio::select! {
-                    Some(score) = score_rx1.recv() => {
-                        let mut clients = clients.lock().await;
-                        // 我们按照游戏周期来发送分数，而不是将分数加和
-                        clients.0.score = score;
-                    },
-                    Some(score) = score_rx2.recv() => {
-                        let mut clients = clients.lock().await;
-                        clients.1.score = score;
-                    },
-                    Some(_) = end_rx1.recv() => {
-                        let clients = clients.lock().await;
-                        if end_rx2.try_recv().is_ok() {
-                            // Announce end of the game
-                            let msg = serde_json::to_string(&GameEnd {
-                                message: "end".to_string(),
-                            }).unwrap();
-                            clients.0.conn.lock().await.send(Message::Text(msg.clone())).await.unwrap();
-                            clients.1.conn.lock().await.send(Message::Text(msg.clone())).await.unwrap();
-                            break;
-                        } else {
-                            let msg = serde_json::to_string(&GameEnd {
-                                message: "wait".to_string(),
-                            }).unwrap();
-                            clients.0.conn.lock().await.send(Message::Text(msg.clone())).await.unwrap();
-                        }
-                    },
-                    Some(_) = end_rx2.recv() => {
-                        let clients = clients.lock().await;
-                        if end_rx1.try_recv().is_ok() {
-                            // Announce end of the game
-                            let msg = serde_json::to_string(&GameEnd {
-                                message: "end".to_string(),
-                            }).unwrap();
-                            clients.0.conn.lock().await.send(Message::Text(msg.clone())).await.unwrap();
-                            clients.1.conn.lock().await.send(Message::Text(msg.clone())).await.unwrap();
-                            break;
-                        } else {
-                            let msg = serde_json::to_string(&GameEnd {
-                                message: "wait".to_string(),
-                            }).unwrap();
-                            clients.1.conn.lock().await.send(Message::Text(msg.clone())).await.unwrap();
-                        }
-                    },
-                }
-                }
-
-                let clients = clients.lock().await;
-                let final_scores = format!(
-                    "Client 1 score: {}, Client 2 score: {}",
-                    clients.0.score,
-                    clients.1.score
-                );
-                println!("{}", final_scores);
+                Session::handle_session(clients, score_rx1, score_rx2, end_rx1, end_rx2).await
             })
         };
 
@@ -215,42 +136,135 @@ impl Session {
             let msg = serde_json
                 ::to_string(
                     &(GameBegin {
-                        message: "begin".to_string(),
+                        begin: "begin".to_string(),
                     })
                 )
                 .unwrap();
             let clients = clients.lock().await;
             clients.0.conn.lock().await.send(Message::Text(msg.clone())).await?;
             clients.1.conn.lock().await.send(Message::Text(msg.clone())).await?;
+            dbg!(msg);
         }
 
         /* ---------- 返回 session ---------- */
 
         Ok((ssid, session))
     }
+
+    /// session 线程函数
+    async fn handle_session(
+        clients: Arc<Mutex<(Client, Client)>>,
+        mut score_rx0: mpsc::UnboundedReceiver<i32>,
+        mut score_rx1: mpsc::UnboundedReceiver<i32>,
+        mut end_rx0: mpsc::UnboundedReceiver<()>,
+        mut end_rx1: mpsc::UnboundedReceiver<()>
+    ) -> Result<(), SessionErr> {
+        let mut cnt: u32 = 2;
+        // polling
+        loop {
+            /* ---------- 分数 ---------- */
+            match score_rx0.try_recv() {
+                Ok(score) => {
+                    let mut clients = clients.lock().await;
+                    clients.0.score = score;
+                    // 向客户端发送分数
+                    let msg = serde_json::to_string(
+                        &(Score {
+                            score,
+                        })
+                    )?;
+                    // 发给对方
+                    clients.1.conn.lock().await.send(Message::Text(msg)).await?;
+                }
+                Err(mpsc::error::TryRecvError::Empty) => {/* 没有数据不是错误 */}
+                Err(e) => {
+                    return Err(e.into());
+                }
+            }
+
+            match score_rx1.try_recv() {
+                Ok(score) => {
+                    let mut clients = clients.lock().await;
+                    clients.1.score = score;
+                    // 向客户端发送分数
+                    let msg = serde_json::to_string(
+                        &(Score {
+                            score,
+                        })
+                    )?;
+                    clients.0.conn.lock().await.send(Message::Text(msg)).await?;
+                }
+                Err(mpsc::error::TryRecvError::Empty) => {/* 没有数据不是错误 */}
+                Err(e) => {
+                    return Err(e.into());
+                }
+            }
+
+            /* ---------- 结束标志 ---------- */
+
+            match end_rx0.try_recv() {
+                Ok(_) => {
+                    cnt -= 1;
+                }
+                Err(mpsc::error::TryRecvError::Empty) => {/* 没有数据不是错误 */}
+                Err(e /* 出现了其他错误 */) => {
+                    return Err(e.into());
+                }
+            }
+
+            match end_rx1.try_recv() {
+                Ok(_) => {
+                    cnt -= 1;
+                }
+                Err(mpsc::error::TryRecvError::Empty) => {/* 没有数据不是错误 */}
+                Err(e /* 出现了其他错误 */) => {
+                    return Err(e.into());
+                }
+            }
+
+            /* ---------- 结束标志 ---------- */
+            if cnt == 0 {
+                // 正常的结束
+                break;
+            }
+        }
+
+        // 宣布游戏结束
+        {
+            let clients = clients.lock().await;
+            let msg0 = serde_json::to_string(
+                &(GameEnd {
+                    end: "end".to_string(),
+                    score: clients.0.score,
+                })
+            )?;
+            clients.1.conn.lock().await.send(Message::Text(msg0.clone())).await?;
+            let msg1 = serde_json::to_string(
+                &(GameEnd {
+                    end: "end".to_string(),
+                    score: clients.0.score,
+                })
+            )?;
+            clients.1.conn.lock().await.send(Message::Text(msg1.clone())).await?;
+        }
+
+        Ok(())
+    }
 }
 
 pub async fn run() -> Result<(), ServerError> {
     let listener = tokio::net::TcpListener::bind("0.0.0.0:9999").await?;
+    dbg!(&listener);
 
     let mut buffer = Vec::new();
     while let Ok((stream, _)) = listener.accept().await {
+        dbg!(&stream);
         let ws_stream = accept_async(stream).await.unwrap();
         buffer.push(ws_stream);
         if buffer.len() >= 2 {
             let (client1_ws, client2_ws) = (buffer.remove(0), buffer.remove(0));
 
             let _ = Session::broadcast_start(client1_ws, client2_ws).await;
-
-            // let session_arc = Arc::new(session);
-            // let session_clone = session_arc.clone();
-            // let handle = tokio::spawn(async move {
-            //     session_clone.run().await;
-            // });
-
-            // session_arc.thread = handle;
-
-            // sessions.lock().await.insert(ssid, session_arc);
         }
     }
     Ok(())
